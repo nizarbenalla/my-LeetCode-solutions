d0b220a4f74787fdae4181bacd90e683482388ea
Nizar Benalla <96922791+NizarBenalla@users.noreply.github.com>
2023-07-18 16:17:33 +0100

    Time: 67 ms (59.93%), Space: 16.9 MB (65.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,16 +3,11 @@ class Solution:
         if len(s)!=len(t) : return False
         d={}
         for i in s:
-            if i not in d :
-                d[i]=0
-            else : 
-                d[i]+=1
+            if i not in d : d[i]=0
+            else : d[i]+=1
         for i in t:
-            if i not in d:
-                return False
-            else :
-                if d[i]<0 :
-                    return False
+            if i not in d: return False
+            else : 
+                if d[i]<0 : return False
                 d[i]-=1
-                
         return True
