7422a0d596929a9c448c68a2961d6563c4423940
Nizar Benalla <96922791+NizarBenalla@users.noreply.github.com>
2023-07-20 11:35:50 +0100

    Time: 239 ms (5.00%), Space: 20.8 MB (41.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if not strs or strs == [""]: return [[""]]
-        d = {}
+        d = defaultdict(list)
         for i in strs:
             l = [0]*26
             for letter in i :
                 l[ord(letter)-ord('a')]+=1
             key = "".join(str(i+ord('a'))+str(l[i]) for i in range(len(l)))
-            if key not in d : d[key]=[]
             d[key].append(i)
         output =[]
         for j in d:
