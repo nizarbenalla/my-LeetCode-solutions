610388b1a0c2ebded9e3b420fc53e17e7964ac38
Nizar Benalla <96922791+NizarBenalla@users.noreply.github.com>
2023-07-20 12:01:17 +0100

    Time: 66 ms (63.58%), Space: 16.8 MB (64.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,14 +2,15 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t) : return False
         d={}
-        for i, j in zip(s, t):
+        for i in s:
+            if i not in d :
+                d[i]=0
+            else : 
+                d[i]+=1
+        for i in t:
             if i not in d:
-                d[i] = 0
-            if j not in d:
-                d[j] = 0
-            d[i] += 1
-            d[j] -= 1
-        for i in d:
-            if d[i] != 0:
                 return False
+            else :
+                if d[i]<0 : return False
+                d[i]-=1
         return True
