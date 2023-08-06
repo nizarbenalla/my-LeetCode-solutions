a9c3eb93b00d9f30cf995ec9d3e9a1cf6bc36781
Nizar Benalla <96922791+NizarBenalla@users.noreply.github.com>
2023-08-06 22:22:15 +0100

    Time: 94 ms (98.46%), Space: 16.4 MB (53.90%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,30 +1,22 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        l=len(board)
         s= set()
         s2=set()
         sq= defaultdict(set)        
-        for i in range(l):
-            for j in range(l):
+        for i in range(9):
+            for j in range(9):
                 v = board[i][j]
-                k=board[j][i]   
-                
+                k=board[j][i]       
                 if v!=".":
                     if v in s or v in sq[(i//3,j//3)] : return False
                     s.add(v)
                     sq[(i//3,j//3)].add(v)
-            
                 if k!=".":
                     if k in s2: return False
-                    s2.add(k)
-                
+                    s2.add(k)  
             s.clear()
             s2.clear()
-            
         s.clear()
-        s2.clear()           
-
-
-        
+        s2.clear()               
         return True
