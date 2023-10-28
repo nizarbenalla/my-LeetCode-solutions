dd20d3bf35172dba5d1d0bbcc1cb02033608bc88
Nizar Benalla <96922791+NizarBenalla@users.noreply.github.com>
2023-10-28 03:27:31 +0100

    Time: 842 ms (69.12%), Space: 27.3 MB (71.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -8,11 +8,10 @@ class Solution(object):
         l,r=0,1
     
         while r < len(prices):
-            
-            if prices[r]-prices[l]>profit:
-                profit=prices[r]-prices[l]
-            
             if prices[r]<prices[l]:
                 l=r
+                continue
+            if prices[r]-prices[l]>profit:
+                profit=prices[r]-prices[l]
             r+=1
         return profit
