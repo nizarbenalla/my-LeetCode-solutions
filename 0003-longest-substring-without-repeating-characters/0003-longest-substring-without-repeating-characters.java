9c75c20303841a8d4154cf23fbf010213982c915
Nizar Benalla <96922791+NizarBenalla@users.noreply.github.com>
2023-11-16 12:10:09 +0100

    Time: 5 ms (84.78%), Space: 43.5 MB (57.30%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,10 +5,10 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (map.containsKey(c)) {
-                right = Math.max(right, map.get(c)+1);
+                right = right> map.get(c)+1? right:map.get(c)+1;
             }
             map.put(c, i);
-            max = Math.max(max, i - right +1);
+            max = max >  i - right +1 ? max :  i - right +1;
         }
         return max;
     }
